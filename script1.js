33492abacfea9f01dbbdc5af3e5017205fab99e3
mudasarmajeed5 <mudasarmajeed5@yahoo.com>
2023-10-31 02:38:58 +0500

    Updated version... I'm working on the light mode.

---
+++ script1.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const socialLinks = document.querySelector('.social-links');
+
+    // Add a class to the social links to trigger the animation
+    socialLinks.classList.add('show');
+});

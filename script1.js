082fdfe3eb8a579ac3c13b1bb287a0beb65d08f5
Mudassar Majeed <mudasarmajeed5@yahoo.com>
2023-10-31 11:51:09 +0500

    style1_animation
    Maybe the

--- script1.js
+++ script1.js
@@ -1,6 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const socialLinks = document.querySelector('.social-links');
+    const socialLinks = document.querySelectorAll('.social-links ul li');
+    const container = document.querySelector('.social-links');
 
-    // Add a class to the social links to trigger the animation
-    socialLinks.classList.add('show');
+    socialLinks.forEach((link, index) => {
+        link.style.transition = `opacity 0.5s ease-in-out ${index * 0.5}s`; // Adjust the delay timing
+        link.style.opacity = 0;
+    });
+
+    container.style.transition = "left 2s, transform 1s";
+
+    function applyAnimation() {
+        if (window.matchMedia("(max-width: 767px)").matches) {
+            container.style.left = "30vw"; // Applied media query: move to 30vw
+        } else {
+            container.style.left = "15vw"; // Default for other resolutions
+        }
+
+        socialLinks.forEach((link, index) => {
+            setTimeout(() => {
+                link.style.opacity = 1;
+            }, 150 + index * 150); // Adjust timing as needed
+        });
+    }
+
+    applyAnimation(); // Initial call for the animation
+
+    window.addEventListener('resize', applyAnimation); // Re-run animation on window resize
 });

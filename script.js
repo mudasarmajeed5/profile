7768bdf67795acb9cd5536b376d78617688d2a31
mudasarmajeed5 <mudasarmajeed5@yahoo.com>
2023-11-08 00:46:38 +0500

    Changes
    Margin is not fixed yet

--- script.js
+++ script.js
@@ -10,3 +10,12 @@
         });
     });
 
+
+    document.getElementById('scrollToTop').addEventListener('click', function(e) {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+    
